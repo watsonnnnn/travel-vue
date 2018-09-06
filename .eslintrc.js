4c8904846ecc96b727c9956d244ea36f213dfe3b
nicewahson <nicelj92@gmail.com>
2018-09-06 12:58:19 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules:{
+        'import/no-unresolved': 0,
+        'no-new': 0
+    }
+};
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import $ from 'jquery';
+
+$.extend({}, { b: 2 });
